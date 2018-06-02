12071ce3a490b92cc0e146d1d06d7740ea5f0058
vayin <deep.ayina@gmail.com>
2018-06-02 03:12:31 +0000

    updated model

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -20,15 +20,15 @@
             <img data-src="<?php echo base_url("assets/images/sb_2.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-2.jpg");?>">
             <div class="carousel-caption">
                 <h3> Carve </h3>
-                <p> Carve your new style on Motion 9000.</p>
+                <p> Carve your new style on Motion 10001.</p>
             </div>
         </div>
 
         <div class="item active" onclick="location.href='<?php echo base_url();?>product'">
             <img data-src="<?php echo base_url("assets/images/sb_4.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-3.jpg");?>">
             <div class="carousel-caption">
-                <h3>Pro Skate Board Motion 10000</h3>
-                <p> Do all your cool stuff on Contoso board Motion 10000.</p>
+                <h3>Pro Skate Board Motion 10001</h3>
+                <p> Do all your cool stuff on Contoso board Motion 10001.</p>
             </div>
         </div>
     </div>
